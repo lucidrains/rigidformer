0bf2ed82a80cd32dfdd2ba7c6006771ba34cfe2b
lucidrains <lucidrains@gmail.com>
2026-05-20 10:18:16 -0700

    attention, but they noted big improvements using gating from john jumper in af2

--- rigidformer/rigidformer.py
+++ rigidformer/rigidformer.py
@@ -1,16 +1,75 @@
+from __future__ import annotations
 
 import torch
-from torch.nn import Module, ModuleList
+from torch.nn import Module, ModuleList, Linear
+
+import einx
+from einops import einsum
+from einops.layers.torch import Rearrange
 
 # helpers
 
 def exists(v):
     return v is not None
 
+def default(v, d):
+    return v if exists(v) else d
+
 # classes
 
+class Attention(Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        dim_inner = dim_head * heads
+        self.scale = dim_head ** -0.5
+
+        self.to_queries_gates = Linear(dim, dim_inner * 2, bias = False)
+        self.to_keys_values = Linear(dim, dim_inner * 2, bias = False)
+
+        self.to_out = Linear(dim_inner, dim)
+
+        self.split_heads = Rearrange('b n (h d) -> b h n d', h = heads)
+        self.merge_heads = Rearrange('b h n d -> b n (h d)')
+
+    def forward(
+        self,
+        tokens,
+        context = None
+    ):
+
+        context = default(context, tokens)
+
+        queries, gates, keys, values = (
+            *self.to_queries_gates(tokens).chunk(2, dim = -1),
+            *self.to_keys_values(context).chunk(2, dim = -1)
+        )
+
+        queries, gates, keys, values = (self.split_heads(t) for t in (queries, gates, keys, values))
+
+        queries = queries * self.scale
+
+        sim = einsum(queries, keys, 'b h i d, b h j d -> b h i j')
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum(attn, values, 'b h i j, b h j d -> b h i d')
+
+        out = out * gates.sigmoid()
+
+        out = self.merge_heads(out)
+        return self.to_out(out)
+
+# main class
+
 class Rigidformer(Module):
-    def __init__(self):
+    def __init__(
+        self
+    ):
         super().__init__()
 
     def forward(
--- tests/test_rigidformer.py
+++ tests/test_rigidformer.py
@@ -2,4 +2,12 @@
 import pytest
 
 def test_rigidformer():
-    assert True
+    from rigidformer.rigidformer import Rigidformer, Attention
+
+    import torch
+
+    x = torch.randn(1, 1024, 512)
+    attn = Attention(512)
+
+    out = attn(x)
+    assert x.shape == out.shape
